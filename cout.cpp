58b772084be853c2a3e2ab4ac96fa2e6c33d527d
yRxf <39730396+yRxf@users.noreply.github.com>
2018-08-04 21:19:43 +0800

    Update cout.cpp

--- cout.cpp
+++ cout.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-	printf("%-5c%-5d%-5f\n", 'c', 123, 12.34567);
+	printf("%-5c%-5d%-5.2f\n", 'c', 123, 12.34567);
 	cout << setiosflags(ios::left) << setw(5) << 'c' << setw(5) << 123 << setw(5) << 12.34567 << endl;
 	//setiosflags(ios::left)   ->   左对齐
 	//setw(5)  ->  最少占五位
-	cout << setiosflags(ios::fixed) << 12.34567 << endl;	// setiosflags(ios::fixed)  ->  按照c格式输出 即保留小数点后6位
+	cout << setiosflags(ios::fixed) << setprecision(2) << 12.34567 << endl;	// setiosflags(ios::fixed)  ->  按照c格式输出  setprecision()设置小数位数
 
 	return 0;
 }
